fb3ef9566b4fc9881d4a74af68d5f4cad21326e1
camilo <camilo.vieira@gmail.com>
2019-11-22 20:05:02 -0300

    Testes com o os.walk

--- scanfiles.py
+++ scanfiles.py
@@ -1,8 +1,31 @@
 #!/usr/bin/python3
 
-import os
-for root, dirs, files in os.walk("/home/camilo/workspace/", topdown=False):
+import os, time, glob, json, itertools, filecmp
+
+files_list = []
+files_remove = []
+#dirs_list = []
+
+files_dict = {}
+
+path = "/home/camilo/workspace/superdigital/clear_images/2019"
+
+for (root, dirs, files) in os.walk(path, topdown=False):
+
    for name in files:
-      print(os.path.join(root, name))
-   for name in dirs:
-       print(os.path.join(root, name))
+      dirs = os.path.join(root)
+      if len(os.listdir(dirs)) > 1:
+         #print(abc + " " + str(len(os.listdir(abc))))
+         files_list.append(os.path.join(root, name))
+
+
+for a in sorted(files_list):
+   #print(a + " " + time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(os.path.getmtime(a))))
+   files_dict[a] = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(os.path.getmtime(a)))
+
+#print("\n",json.dumps(files_dict))
+
+
+for f1, f2 in itertools.combinations(files_list, 2):
+    if filecmp.cmp(f1, f2):
+        print(f1, f2)
