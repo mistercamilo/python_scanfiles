3132c6e6e83d4cb0a98a5bafe9be4efa6f7d7edf
camilo <camilo.vieira@gmail.com>
2019-11-25 18:34:48 -0300

    Inclusao do ano como parametro

--- scanfiles.py
+++ scanfiles.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
 
-import os, time, glob, json, itertools, filecmp
+import os, time, sys
+
+year = sys.argv[1]
 
 files_list = []
 dirs_list = []
 
-path = "/home/camilo/workspace/python/2019"
+path = "/home/camilo/workspace/superdigital/python_scanfiles/" + year
 
 for (root, dirs, files) in os.walk(path, topdown=False):
    for name in files:
@@ -17,8 +19,10 @@ dirs_list_unique = (set(dirs_list))
 
 for root in sorted(dirs_list_unique):
     for b in os.listdir(root):
-        if os.path.isfile(os.path.join(root, b)):
-            files_list.append(os.path.join(root, b))
+       if os.path.isfile(os.path.join(root, b)) and b.endswith(".txt"):
+          files_list.append(os.path.join(root, b))
+       else:
+          continue
 
     files_list = sorted(files_list, key=os.path.getmtime)
 
@@ -26,4 +30,4 @@ for root in sorted(dirs_list_unique):
 
     for files2del in files_list:
         os.remove(files2del)
-    files_list.clear()    
+    files_list.clear()
